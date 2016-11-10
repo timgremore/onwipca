d4bf3b8909cf50854dc471aaeb334f73b78c0e04
Tim Gremore <tim@entrision.com>
2016-11-10 06:38:02 -0600

    Gray box for a logo

--- web/static/js/components/main_layout.js
+++ web/static/js/components/main_layout.js
@@ -4,7 +4,10 @@ class MainLayout extends Component {
   render() {
     return (
       <div className="o-layout o-layout--center">
-        <div className="o-layout__item u-1/2">
+        <div className="o-layout__item u-1/1">
+          <img src="/images/logo.svg" />
+        </div>
+        <div className="o-layout__item u-2/3">
           {this.props.children}
         </div>
       </div>
