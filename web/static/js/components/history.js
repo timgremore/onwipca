b47bca6e6262ff3cf620498786b83fc40b880ae5
Tim Gremore <tim@entrision.com>
2016-11-12 08:19:31 -0600

    Add image and copy to history section

--- web/static/js/components/history.js
+++ web/static/js/components/history.js
@@ -4,7 +4,41 @@ class History extends Component {
   render() {
     return (
       <section id="history">
-        <h1 className="u-copy-cursive">History</h1>
+        <div className="o-wrapper">
+          <div className="o-layout o-layout--center">
+            <div className="o-layout__item u-1/1">
+              <h1 className="u-copy-cursive u-text-center">History</h1>
+            </div>
+            <div className="o-layout__item u-2/5">
+              <p className="u-text-center">
+                <img src="/images/wisconsin.svg" width="350" />
+              </p>
+            </div>
+            <div className="o-layout__item u-2/5">
+              <p>The Wisconsin presbytery was composed of six particularized churches from 1992 until 2013:</p>
+              <ul>
+                <li>
+                  Christ Covenant Church, <em>La Crosse, WI</em>
+                </li>
+                <li>
+                  Grace Presbyterian Church, <em>Pardeeville, WI</em>
+                </li>
+                <li>
+                  Lake Trails Church, <em>Madison, WI</em>
+                </li>
+                <li>
+                  Bible Presbyterian Church, <em>Merrill, WI</em>
+                </li>
+                <li>
+                  Trinity Presbyterian Church, <em>Waukesha, WI</em>
+                </li>
+                <li>
+                  Cornerstone Presbyterian Church, <em>Delafield, WI</em>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </section>
     )
   }
