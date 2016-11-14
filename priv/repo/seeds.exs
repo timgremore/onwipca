1ba2d298fa06081ac65693b517ee5c0a8ac1e762
Tim Gremore <tim@entrision.com>
2016-11-14 08:06:07 -0600

    Add a couple church records to seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,28 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Onwipca.Repo
+alias Onwipca.Church
+
+church = %Church{
+    name: "Christ Covenant Church",
+    street: "3630 Co Rd B",
+    city: "La Crosse",
+    state: "WI",
+    zipcode: "54601",
+    particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12, min: 0, sec: 0}
+  }
+  |> Repo.insert!
+
+church = %Church{
+    name: "Grace Presbyterian Church",
+    street: "N7631 WI-22",
+    city: "Pardeeville",
+    state: "WI",
+    zipcode: "53954",
+    particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12, min: 0, sec: 0}
+  }
+  |> Repo.insert!
+
+# TODO: Add 4 other of the 6 churches that were particularized between 1992 and 2013
