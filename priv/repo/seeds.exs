dfd64ad47bc597e53537fc6a9e17bddbf552fe91
Daniel Humbert <achristian4truth@yahoo.com>
2016-11-19 11:19:13 -0600

    add 4 churches to history

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -33,4 +33,49 @@ church = %Church{
   }
   |> Repo.insert!
 
+church = %Church{
+  name: "Lake Trails Church",
+  street: "1 Point Place Suite 1",
+  city: "Madison",
+  state: "WI",
+  zipcode: "53719",
+  particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12,
+  min: 0, sec: 0}
+}
+|> Repo.insert!
+
+church = %Church{
+  name: "Bible Presbyterian Church",
+  street: "1605 Hwy G",
+  city: "Merrill",
+  state: "WI",
+  zipcode: "54452",
+  particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12,
+  min: 0, sec: 0}
+}
+|> Repo.insert!
+
+church = %Church{
+  name: "Trinity Presbyterian Church",
+  street: "224 N.Hine Ave",
+  city: "Waukesha",
+  state: "WI",
+  zipcode: "53188",
+  particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12,
+  min: 0, sec: 0}
+}
+|> Repo.insert!
+
+church = %Church{
+  name: "Cornerstone Presbyterian Church",
+  street: "N6 W31449 Alberta Drive",
+  city: "Delafield",
+  state: "WI",
+  zipcode: "53018",
+  particularized_at: %Ecto.DateTime{year: 1992, month: 1, day: 1, hour: 12,
+  min: 0, sec: 0}
+}
+|> Repo.insert!
+
+
 # TODO: Add 4 other of the 6 churches that were particularized between 1992 and 2013
