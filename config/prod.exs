eaf782b0051bb0bd9fd050b7d144ff4caef33249
Tim Gremore <tim@entrision.com>
2016-11-21 20:58:30 -0600

    Fix Heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -62,7 +62,7 @@ config :logger, level: :info
 # import_config "prod.secret.exs"
 
 # Configure your database
-config :hello_phoenix, Onwipca.Repo,
+config :onwipca, Onwipca.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
