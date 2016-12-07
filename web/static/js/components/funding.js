98e6ba453e41ec6a98b8ddc7364b6386eb19d9a8
Daniel Humbert <achristian4truth@yahoo.com>
2016-12-06 20:47:10 -0600

    funding section inital build

--- web/static/js/components/funding.js
+++ web/static/js/components/funding.js
@@ -4,8 +4,30 @@ class Funding extends Component {
   render() {
     return (
       <section id="funding">
-        <h1>Funding</h1>
-        <h2>content</h2>
+        <div className="">
+          <h1 className="">Funding</h1>
+        </div>
+        <div className="">
+          <h2 className="">We need outside partners to help fund the "On Wisconsin" network.</h2>
+        </div>
+        <div className="">
+          <h2 className="">To maximize a $100,000 grant opportunity</h2>
+        </div>
+        <div className="">
+          <h2 className="">To fully fund the "On Wisconsin" network, we need 15% from outside sources. </h2>
+        </div>
+        /* need to insert image of pie chart */
+        <div className="">
+          <h3 className="">Funding from Wisconsin Presbytery: 75%</h3>
+        </div>
+        <div className="">
+          <h3 className="">Funding from grant: 10%</h3>
+        </div><div className="">
+          <h3 className="">Funding from churches/networks outside of Wisconsin: 10%</h3>
+        </div><div className="">
+          <h3 className="">Funding from individuals: 5%</h3>
+        </div>
+
       </section>
     )
   }
