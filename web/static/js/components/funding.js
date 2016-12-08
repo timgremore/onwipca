a7ff72f2d3031bf21630d19aaa0827431f591733
Daniel Humbert <achristian4truth@yahoo.com>
2016-12-07 20:11:15 -0600

    added content to funding

--- web/static/js/components/funding.js
+++ web/static/js/components/funding.js
@@ -4,30 +4,52 @@ class Funding extends Component {
   render() {
     return (
       <section id="funding">
-        <div className="">
-          <h1 className="">Funding</h1>
+        <div className="o-wrapper">
+          <div className="o-layout o-layout--center">
+            <div className="o-layout__item u-1/1">
+              <h1 className="u-copy-cursive u-text-center">Funding</h1>
+            </div>
+            <div className="">
+              <h2 className="">Wisconsin’s churches commitment to church planting</h2>
+            </div>
+            <div className="">
+              <h3 className="">Our financial commitment as the Wisconsin Presbytery:</h3>
+            </div>
+            <div className="">
+              <h3 className="">Wisconsin churches have already given over $200,000
+              to church planting in Wisconsin since 2013</h3>
+            </div>
+            <div className="">
+              <h3 className="">100% of our Wisconsin churches are invested in the “On Wisconsin” Program</h3>
+            </div>
+            <div className="">
+              <h3 className="">Wisconsin churches are giving $100,000+ per year with an annual 10% increase</h3>
+            </div>
+            <div className="">
+              <h3 className="">By 2022 the “On Wisconsin” network will be fully funded by the Wisconsin Presbytery</h3>
+            </div>
+            <div className="">
+              <h2 className="">We need outside partners to help fund the "On Wisconsin" network.</h2>
+            </div>
+            <div className="">
+              <h2 className="">To maximize a $100,000 grant opportunity</h2>
+            </div>
+            <div className="">
+              <h2 className="">To fully fund the "On Wisconsin" network, we need 15% from outside sources. </h2>
+            </div>
+            /* need to insert image of pie chart */
+            <div className="">
+              <h3 className="">Funding from Wisconsin Presbytery: 75%</h3>
+            </div>
+            <div className="">
+              <h3 className="">Funding from grant: 10%</h3>
+            </div><div className="">
+              <h3 className="">Funding from churches/networks outside of Wisconsin: 10%</h3>
+            </div><div className="">
+              <h3 className="">Funding from individuals: 5%</h3>
+            </div>
+          </div>
         </div>
-        <div className="">
-          <h2 className="">We need outside partners to help fund the "On Wisconsin" network.</h2>
-        </div>
-        <div className="">
-          <h2 className="">To maximize a $100,000 grant opportunity</h2>
-        </div>
-        <div className="">
-          <h2 className="">To fully fund the "On Wisconsin" network, we need 15% from outside sources. </h2>
-        </div>
-        /* need to insert image of pie chart */
-        <div className="">
-          <h3 className="">Funding from Wisconsin Presbytery: 75%</h3>
-        </div>
-        <div className="">
-          <h3 className="">Funding from grant: 10%</h3>
-        </div><div className="">
-          <h3 className="">Funding from churches/networks outside of Wisconsin: 10%</h3>
-        </div><div className="">
-          <h3 className="">Funding from individuals: 5%</h3>
-        </div>
-
       </section>
     )
   }
