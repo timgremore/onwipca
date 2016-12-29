7d957b1fdf87c153375b4e9ecbc02bb5fb7f5639
Tim Gremore <tim@headway.io>
2016-12-29 07:50:44 -0600

    Move pathways panel to the right side

--- web/static/js/components/pathways_map.js
+++ web/static/js/components/pathways_map.js
@@ -7,7 +7,7 @@ import { selectStage } from '../actions/churches'
 
 class PathwaysMap extends Component {
   render() {
-    const position = [44.7844, -91.7879]
+    const position = [44.7844, -87.0]
     const zoom = 7
     const attribution = "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>"
     const churches = filter(this.props.churches, { 'stage': this.props.selectedStage })
@@ -30,7 +30,7 @@ class PathwaysMap extends Component {
               url="https://api.mapbox.com/styles/v1/timgremore/ciwgbl364007q2qoih4a4wb4a/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGltZ3JlbW9yZSIsImEiOiJtY01hT2lNIn0.jS2hKzkpWj83rJSrlOz6vg"
               attribution={attribution} />
             {markers}
-            <div className="c-panel u-1/4 u-padding">
+            <div className="c-panel u-1/3 u-padding">
               <div className="c-accordion">
                 <h1 className="u-text-center">Pathways</h1>
                 <ul className="o-list-bare">
