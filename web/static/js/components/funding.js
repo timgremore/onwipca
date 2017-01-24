7932c923c66d80c79b135cb48190e977c67b55ea
Tim Gremore <tim@headway.io>
2017-01-24 07:10:06 -0600

    Upgrade React and install Recharts

--- web/static/js/components/funding.js
+++ web/static/js/components/funding.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 
+import FundingChart from './funding_chart'
+
 class Funding extends Component {
   render() {
     return (
@@ -9,44 +11,20 @@ class Funding extends Component {
             <div className="o-layout__item u-1/1">
               <h1 className="u-copy-cursive u-text-center">Funding</h1>
             </div>
-            <div className="">
-              <h2 className="">Wisconsin’s churches commitment to church planting</h2>
-            </div>
-            <div className="">
-              <h3 className="">Our financial commitment as the Wisconsin Presbytery:</h3>
-            </div>
-            <div className="">
-              <h3 className="">Wisconsin churches have already given over $200,000
-              to church planting in Wisconsin since 2013</h3>
-            </div>
-            <div className="">
-              <h3 className="">100% of our Wisconsin churches are invested in the “On Wisconsin” Program</h3>
-            </div>
-            <div className="">
-              <h3 className="">Wisconsin churches are giving $100,000+ per year with an annual 10% increase</h3>
-            </div>
-            <div className="">
-              <h3 className="">By 2022 the “On Wisconsin” network will be fully funded by the Wisconsin Presbytery</h3>
-            </div>
-            <div className="">
-              <h2 className="">We need outside partners to help fund the "On Wisconsin" network</h2>
-            </div>
-            <div className="">
-              <h2 className="">To maximize a $100,000 grant opportunity</h2>
-            </div>
-            <div className="">
+            <div className="o-layout__item u-1/2">
+              <h2>Wisconsin&rsquo;s churches commitment to church planting</h2>
+              <p>Our financial commitment as the Wisconsin Presbytery:</p>
+              <ul>
+                <li>Wisconsin churches have already given over $200,000 to church planting in Wisconsin since 2013</li>
+                <li>100% of our Wisconsin churches are invested in the &ldquo;On Wisconsin&rdquo; Program</li>
+                <li>Wisconsin churches are giving $100,000+ per year with an annual 10% increase</li>
+                <li>By 2022 the &ldquo;On Wisconsin&rdquo; network will be fully funded by the Wisconsin Presbytery</li>
+              </ul>
+              <p>We need outside partners to help fund the &ldquo;On Wisconsin&rdquo; network to maximize a $100,000 grant opportunity</p>
               <h2 className="">To fully fund the "On Wisconsin" network, we need 15% from outside sources</h2>
             </div>
-            /* need to insert image of pie chart */
-            <div className="">
-              <h3 className="">Funding from Wisconsin Presbytery: 75%</h3>
-            </div>
-            <div className="">
-              <h3 className="">Funding from grant: 10%</h3>
-            </div><div className="">
-              <h3 className="">Funding from churches/networks outside of Wisconsin: 10%</h3>
-            </div><div className="">
-              <h3 className="">Funding from individuals: 5%</h3>
+            <div className="o-layout__item u-1/2">
+              <FundingChart />
             </div>
           </div>
         </div>
---
+++ web/static/js/components/funding_chart.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react"
+import { PieChart, Pie, Sector, Cell } from 'recharts';
+
+const data = [
+  {name: 'Wisconsin Presbytery', value: 75},
+  {name: 'Grants', value: 10},
+  {name: 'Churches/networks outside Wisconsin', value: 10},
+  {name: 'Individuals', value: 5}
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+ 	const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x  = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy  + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} 	dominantBaseline="central">
+    	{`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+
+class FundingChart extends Component {
+  render() {
+    return (
+    	<PieChart width={800} height={400} onMouseEnter={this.onPieEnter}>
+        <Pie
+          data={data}
+          cx={300}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8">
+        	{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+        </Pie>
+      </PieChart>
+    )
+  }
+}
+
+export default FundingChart
