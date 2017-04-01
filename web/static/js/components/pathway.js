ea6dec250c1d8c1d748e704b3ecf2e045239940d
Tim Gremore <tim@headway.io>
2017-04-01 17:26:03 -0500

    Render markdown

--- web/static/js/components/pathway.js
+++ web/static/js/components/pathway.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react"
 import { connect } from 'react-redux'
+import showdown from 'showdown'
 
 import { selectPathway } from '../actions/pathways'
+
+const converter = new showdown.Converter()
 
 class Pathway extends Component {
   render() {
     const { pathway, selectedPathway } = this.props
+
+    const readiness = converter.makeHtml(pathway.readiness)
+    const location  = converter.makeHtml(pathway.location)
+    const training  = converter.makeHtml(pathway.training)
+    const sender    = converter.makeHtml(pathway.sender)
 
     return (
       <li>
@@ -17,22 +25,17 @@ class Pathway extends Component {
         <i></i>
         <h3 className="u-margin-bottom-none">{pathway.name}</h3>
         <div>
-          <p className="u-margin-bottom-tiny">
-            <strong>Church Planter Readiness:</strong><br />
-            {pathway.readiness}
-          </p>
-          <p className="u-margin-bottom-tiny">
-            <strong>Probable Location:</strong><br />
-            {pathway.location}
-          </p>
-          <p className="u-margin-bottom-tiny">
-            <strong>Training:</strong><br />
-            {pathway.training}
-          </p>
-          <p className="u-margin-bottom-large">
-            <strong>Sender:</strong><br />
-            {pathway.sender}
-          </p>
+          <p className="u-margin-bottom-none"><strong>Church Planter Readiness:</strong></p>
+          <div dangerouslySetInnerHTML={{ __html: readiness}} />
+
+          <p className="u-margin-bottom-none"><strong>Probable Location:</strong></p>
+          <div dangerouslySetInnerHTML={{ __html: location}} />
+
+          <p className="u-margin-bottom-none"><strong>Training:</strong></p>
+          <div dangerouslySetInnerHTML={{ __html: training}} />
+
+          <p className="u-margin-bottom-none"><strong>Sender:</strong></p>
+          <div dangerouslySetInnerHTML={{ __html: sender}} />
         </div>
       </li>
     )
