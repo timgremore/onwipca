8075f1437c23f65475a3a23128533b267252d949
Tim Gremore <tim@headway.io>
2017-04-01 16:12:19 -0500

    Fix layout bug associated with hero transform

--- web/static/js/components/funding.js
+++ web/static/js/components/funding.js
@@ -12,7 +12,7 @@ class Funding extends Component {
               <h1 className="u-text-center">Funding</h1>
               <p className="o-lede u-text-center">Wisconsin&rsquo;s churches commitment to church planting</p>
             </div>
-            <div className="o-layout__item u-1/3">
+            <div className="o-layout__item u-1/1 u-1/3@tablet">
               <h5 className="u-text-center">Our financial commitment as the Wisconsin Presbytery</h5>
               <ul>
                 <li>Wisconsin churches have already given over $200,000 to church planting in Wisconsin since 2013</li>
@@ -21,10 +21,10 @@ class Funding extends Component {
                 <li>By 2022 the &ldquo;On Wisconsin&rdquo; network will be fully funded by the Wisconsin Presbytery</li>
               </ul>
             </div>
-            <div className="o-layout__item u-1/3">
+            <div className="o-layout__item u-1/1 u-1/3@tablet">
               <FundingChart />
             </div>
-            <div className="o-layout__item u-1/3">
+            <div className="o-layout__item u-1/1 u-1/3@tablet">
               <h5 className="u-text-center">How you can help</h5>
               <ul>
                 <li>We need outside partners to help fund the &ldquo;On Wisconsin&rdquo; network to maximize a $100,000 grant opportunity</li>
