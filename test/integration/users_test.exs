ec005503be863b25b62d5d0368d2f43ca06e04e9
Tim Gremore <tim@headway.io>
2017-05-11 21:52:56 -0500

    User management test

--- test/integration/users_test.exs
+++ test/integration/users_test.exs
@@ -22,22 +22,25 @@ defmodule Onwipca.UsersTest do
 
     click({:link_text, "Create User"})
 
-    fill_field({:id, "user_first_name"}, "John")
-    fill_field({:id, "user_last_name"}, "Baptist")
-    fill_field({:id, "user_email"}, "john@onwipca.com")
+    fill_field({:id, "user_first_name"}, "Peter")
+    fill_field({:id, "user_last_name"}, "Apostle")
+    fill_field({:id, "user_username"}, "pete")
+    fill_field({:id, "user_email"}, "pete@onwipca.com")
     fill_field({:id, "user_password"}, "secret")
+    click({:tag, "button"})
 
-    assert page_source =~ "User created successfully"
-    assert page_source =~ "John Baptist"
+    assert page_source =~ "Peter"
+    assert page_source =~ "Apostle"
+    assert page_source =~ "pete@onwipca.com"
 
     click({:link_text, "Logout"})
 
     click({:link_text, "Login"})
 
-    fill_field({:id, "user_username"}, "jb")
+    fill_field({:id, "user_username"}, "pete")
     fill_field({:id, "user_password"}, "secret")
     submit_element({:id, "login-form"})
 
-    assert page_source =~ "Account"
+    assert page_source =~ "Welcome Peter Apostle"
   end
 end
